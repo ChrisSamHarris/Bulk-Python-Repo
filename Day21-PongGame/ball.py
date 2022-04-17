7aae0c247bff1efd55b73ca1b66033daa2e5d627
ChrisSamHarris <christopherharris1995@ymail.com>
2022-04-17 16:49:50 +0100

    added scoreboard and reset system with the ball moving in the opposite direction once it has passed a paddle.

--- Day21-PongGame/ball.py
+++ Day21-PongGame/ball.py
@@ -10,11 +10,19 @@ class Ball(Turtle):
         self.x_move = 5
         self.y_move = 5
         self.setheading(270)
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move #X co-ordinates keep the ball moving forward
         new_y = self.ycor() + self.y_move #Y co-ordinates set the angle at which the ball moves
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1 #multiply by minus one to alter between +10 and -10
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_y()
