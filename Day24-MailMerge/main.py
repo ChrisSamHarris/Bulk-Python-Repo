e9dca0150cec20e7b73aaf8aee00e3e6e65b2f1e
ChrisSamHarris <christopherharris1995@ymail.com>
2022-04-23 14:59:45 +0100

    Added fore context for visibility

--- Day24-MailMerge/main.py
+++ Day24-MailMerge/main.py
@@ -10,20 +10,22 @@
 PLACEHOLDER = "[name]"
 
 with open("./Input/Names/invited_names.txt") as names_file:
-    names = names_file.readlines()
+    names = names_file.readlines() # Returns a list containing each line in the file as a list item
     print(names)
 
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
-        stripped_name = name.strip() #removes the white space and unnecessary new lines between the name and the letter content
+        stripped_name = name.strip() #removes the white space and unnecessary new lines/space and \n between the name and the letter content
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
         #print(new_letter)
         #Now need to generate a new letter with the respective name for who it is addressed - required fstring and write mode to generate the letters
         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
 
-
+string_with_n = "Test String \n        "
+ammended_string = string_with_n.strip()
+print(ammended_string)
 
 # stringToEdit = "I like bananas, oranges and pineapples"
 # editedString = stringToEdit.replace("pineapples", "pears")
